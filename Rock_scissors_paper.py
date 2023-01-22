420467c987051146c991233afddcfe641b4f918e
MarielaYovkova <marielka23@gmail.com>
2023-01-22 18:47:54 +0200

    amended code in line 6,8 and 10

--- Rock_scissors_paper.py
+++ Rock_scissors_paper.py
@@ -3,31 +3,29 @@ import random
 player_move = input('Choose [r]ock,[p]aper or [s]cissors:')
 
 if player_move == 'r':
-    player_move = 'Rock'
+    player_move = 'rock'
 elif player_move == 'p':
-    player_move = 'Paper'
+    player_move = 'paper'
 elif player_move == 's':
-    player_move = 'Scissors'
+    player_move = 'scissors'
 else:
     raise SystemExit('Invalid input!Try again.')
 
 computer_chose = random.randint(1, 3)
-if computer_chose==1:
-    computer_move='rock'
-elif computer_chose==2:
-    computer_move='scissors'
-elif computer_chose==3:
-    computer_move='paper'
+if computer_chose == 1:
+    computer_move = 'rock'
+elif computer_chose == 2:
+    computer_move = 'scissors'
+else:
+    computer_move = 'paper'
 
 print(f'The computer chose {computer_move}')
-
-computer_move = ''
 
 if (player_move == 'rock' and computer_move == 'scissors') or \
         (player_move == 'paper' and computer_move == 'rock') or \
         (player_move == 'scissors' and computer_move == 'paper'):
     print('You win!')
-elif player_move==computer_move:
+elif player_move == computer_move:
     print('Draw!')
 else:
     print('You lose!')
